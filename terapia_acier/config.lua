c05f480de5e916b388b06d48ffaa2b8c7e8039f5
Aynetix <158204955+Aynetix@users.noreply.github.com>
2024-01-31 07:19:45 +0100

    Update config.lua

--- terapia_acier/config.lua
+++ terapia_acier/config.lua
@@ -17,3 +17,10 @@ traitementacier      = {Pos = { x = 981.442,  y = -1921.300, z = 31.134 }}
 Config.reventeacier = {
 reventeacier      = {Pos = { x = 1189.738, y = -3107.030, z =  5.565 }}
 }
+
+Config.Map = {
+  {name="acier:recolte",color=46, id=237,x = 99.017,  y = -386.931,  z = 41.279},
+  {name="acier:traitement",color=46, id=237, x=981.743, y=-1921.560, z=31.134},
+  {name="acier:vente",color=46, id=237, x=1188.915, y=-3106.542, z=5.599}
+
+}
